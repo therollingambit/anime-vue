57a1889920503c6d014113d8c2850d0af842efb3
Ahmad Enayathullah <ahmad.enayathullah@fdmgroup.com>
2021-05-31 17:43:58 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,158 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <header>
+      <h1>The <strong>Anime</strong>DB</h1>
+      <form class="search-box" @submit.prevent="handleSearch">
+        <input
+          type="search"
+          class="search-field"
+          placeholder="Search for an anime..."
+          required
+          v-model="search_query"
+        />
+      </form>
+    </header>
+
+    <main>
+      <h2 v-if="searchTitle.length > 0">Search for "{{ search_query }}"</h2>
+      <div class="cards">
+        <Card v-for="anime in animeList" :key="anime.mal_id" :anime="anime" />
+      </div>
+
+      <div class="no-results" v-if="animeList.length === 0">
+        <h3>Sorry, we have no results...</h3>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from "@/components/Card";
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Card },
+  data() {
+    return {
+      search_query: "",
+      animeList: [],
+      searchTitle: "",
+    };
+  },
+  created() {
+    this.fetchInit();
+  },
+  methods: {
+    async handleSearch() {
+      const res = await axios.get(
+        `https://api.jikan.moe/v3/search/anime?q=${this.search_query}`
+      );
+      const data = await res.data.results;
+      this.animeList = data;
+      this.searchTitle = this.search_query;
+    },
+    async fetchInit() {
+      const res = await axios.get(
+        `https://api.jikan.moe/v3/search/anime?q=demon_slayer`
+      );
+      const data = await res.data.results;
+      this.animeList = data;
+      this.search_query = "";
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url("https://fonts.googleapis.com/css2?family=Fira+Sans&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+
+  font-family: "Fira Sans", sans-serif;
 }
+
+a {
+  text-decoration: none;
+}
+
+header {
+  padding-top: 50px;
+  padding-bottom: 50px;
+
+  h1 {
+    color: #888;
+    font-size: 42px;
+    font-weight: 400;
+    text-align: center;
+    text-transform: uppercase;
+    margin-bottom: 30px;
+
+    strong {
+      color: #313131;
+    }
+
+    &:hover {
+      color: #313131;
+    }
+  }
+
+  .search-box {
+    display: flex;
+    justify-content: center;
+    padding: 0 30px;
+
+    .search-field {
+      appearance: none;
+      background: none;
+      border: none;
+      outline: none;
+      background-color: #f3f3f3;
+      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.15);
+
+      display: block;
+      width: 100%;
+      max-width: 600px;
+      padding: 15px;
+      border-radius: 8px;
+
+      color: #313131;
+      font-size: 20px;
+
+      transition: 0.4s;
+
+      &::placeholder {
+        color: #aaa;
+      }
+
+      &:focus,
+      &:valid {
+        color: #fff;
+        background-color: #313131;
+        box-shadow: 0 0 0 rgba(0, 0, 0, 0.15);
+      }
+    }
+  }
+}
+
+main {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 30px;
+
+  .cards {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0 -8px;
+    justify-content: space-between;
+  }
+
+  h2 {
+    padding-bottom: 20px;
+  }
+}
+
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="card">
+    <img :src="anime.image_url" :alt="anime.title" />
+    <h3>{{ anime.title }}</h3>
+    <p>
+      Score: <span>{{ anime.score }}</span>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: ["anime"],
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  flex: 0 1 31%;
+  flex-wrap: wrap;
+  width: 31%;
+  max-width: 31%;
+  padding: 0 8px;
+  margin-bottom: 18px;
+
+  img {
+    width: 100%;
+    height: 300px;
+    object-fit: cover;
+    border-radius: 16px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.15);
+    transition: 0.4s;
+  }
+
+  h3 {
+    color: #313131;
+    padding: 10px;
+    transition: 0.4s;
+    text-align: center;
+  }
+
+  p {
+    color: #888;
+    transition: 0.4s;
+    text-align: center;
+
+    span {
+      color: #81a1c1;
+    }
+  }
+
+  &:hover {
+    img {
+      transform: scale(1.05);
+    }
+
+    h3 {
+      color: #24c444;
+    }
+  }
+}
+</style>
